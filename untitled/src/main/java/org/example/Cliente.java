46b37362ff612fbd107953ee6d4a0b1d435e89c0
Administrador <thhyagofoz@gmail.com>
2023-03-15 18:56:20 -0300

    criei os atributos da classe cliente e criei um novo enum

--- untitled/src/main/java/org/example/Cliente.java
+++ untitled/src/main/java/org/example/Cliente.java
@@ -31,6 +31,69 @@ public class Cliente extends Entitty{
         this.pontoReferencia = pontoReferencia;
     }
 
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public TipoDeLogradouro getTipoLogradouro() {
+        return tipoLogradouro;
+    }
+
+    public void setTipoLogradouro(TipoDeLogradouro tipoLogradouro) {
+        this.tipoLogradouro = tipoLogradouro;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public int getNumResidencia() {
+        return numResidencia;
+    }
+
+    public void setNumResidencia(int numResidencia) {
+        this.numResidencia = numResidencia;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getPontoReferencia() {
+        return pontoReferencia;
+    }
+
+    public void setPontoReferencia(String pontoReferencia) {
+        this.pontoReferencia = pontoReferencia;
+    }
+
 
 
 }

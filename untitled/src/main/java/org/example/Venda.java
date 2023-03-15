cf035ec421268c927fc0f3d051134b92713eba26
Rafael Ch <rafael.cherman0@gmail.com>
2023-03-15 17:43:02 -0300

    adicionado atributos na classe Venda

--- untitled/src/main/java/org/example/Venda.java
+++ untitled/src/main/java/org/example/Venda.java
@@ -1,4 +1,22 @@
 package org.example;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class Venda {
+
+
+    private BigDecimal total;
+
+    private boolean validacaoPagamento;
+
+    //private Pagamento formaPagamento;
+
+    private LocalDateTime dataDaEntrega;
+
+    private LocalDateTime dataDaVenda;
+
+    private boolean confirmacaoDaVenda;
+
+    private boolean confirmcacaoDaEntrega;  
 }

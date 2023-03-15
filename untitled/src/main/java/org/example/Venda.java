650c783b39c79fcdce0d3e4f12764196f50660c5
Rafael Ch <rafael.cherman0@gmail.com>
2023-03-15 20:46:02 -0300

    gets and sets

--- untitled/src/main/java/org/example/Venda.java
+++ untitled/src/main/java/org/example/Venda.java
@@ -19,4 +19,53 @@ public class Venda {
     private boolean confirmacaoDaVenda;
 
     private boolean confirmcacaoDaEntrega;
+
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public boolean isValidacaoPagamento() {
+        return validacaoPagamento;
+    }
+
+    public void setValidacaoPagamento(boolean validacaoPagamento) {
+        this.validacaoPagamento = validacaoPagamento;
+    }
+
+    public LocalDateTime getDataDaEntrega() {
+        return dataDaEntrega;
+    }
+
+    public void setDataDaEntrega(LocalDateTime dataDaEntrega) {
+        this.dataDaEntrega = dataDaEntrega;
+    }
+
+    public LocalDateTime getDataDaVenda() {
+        return dataDaVenda;
+    }
+
+    public void setDataDaVenda(LocalDateTime dataDaVenda) {
+        this.dataDaVenda = dataDaVenda;
+    }
+
+    public boolean isConfirmacaoDaVenda() {
+        return confirmacaoDaVenda;
+    }
+
+    public void setConfirmacaoDaVenda(boolean confirmacaoDaVenda) {
+        this.confirmacaoDaVenda = confirmacaoDaVenda;
+    }
+
+    public boolean isConfirmcacaoDaEntrega() {
+        return confirmcacaoDaEntrega;
+    }
+
+    public void setConfirmcacaoDaEntrega(boolean confirmcacaoDaEntrega) {
+        this.confirmcacaoDaEntrega = confirmcacaoDaEntrega;
+    }
 }

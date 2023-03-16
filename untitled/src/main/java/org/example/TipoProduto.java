244ea3c54692a3072664cb7ccc88cf3ed3e34e01
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2023-03-16 19:21:51 -0300

    feito a classe do tipo de produto

--- untitled/src/main/java/org/example/TipoProduto.java
+++ untitled/src/main/java/org/example/TipoProduto.java
@@ -1,4 +1,34 @@
 package org.example;
 
 public class TipoProduto {
+    private String genero;
+    private String tipoProduto;
+    private String estacao;
+
+    public String getGenero(){
+        return genero;
+    }
+
+    public String getTipoProduto(){
+        return tipoProduto;
+    }
+
+    public String getEstacao() {
+        return estacao;
+    }
+
+    public void setGenero (String genero){
+        this.genero = genero;
+    }
+
+    public void setTipoProduto(String tipoProduto){
+        this.tipoProduto = tipoProduto;
+    }
+
+    public void setEstacao(String estacao) {
+        this.estacao = estacao;
+    }
+
+
+
 }
